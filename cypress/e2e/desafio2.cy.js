2ff90d9e05689c0c5e51369057f0bae4324850ad
moenni <nicolasmoen@live.com>
2024-02-23 23:49:14 -0300

    Desafio 2 finalizado

--- cypress/e2e/desafio2.cy.js
+++ cypress/e2e/desafio2.cy.js
@@ -11,10 +11,9 @@ describe('Desarrollo Desafio 2',()=>{
        cy.visit('')
        cy.get('#registertoggle').dblclick()
        cy.get('#user').type(Cypress.env().username)
-       cy.get('#pass').type(Cypress.env().password)
+       cy.xpath("//input[@id='pass']").type(Cypress.env().password)
        cy.get('#submitForm').click()
-       cy.get('#todolistlink').click()
-       cy.wait(5000)
+       cy.get('#todolistlink',{timeout:10000}).click()
        cy.get('[data-cy="removeAll"]').click()
    })
    it('Agregar tareas',()=>{
@@ -36,10 +35,31 @@ describe('Desarrollo Desafio 2',()=>{
     cy.get('#task').wait(2000).type(data.tareas.tarea5)
     cy.get('#sendTask').click()
    })
-   it.only ('Verificar botones',()=>{
+   it('Verificar botones',()=>{
     cy.get('#all').should('exist')
     cy.get('#completed').should('exist')
     cy.get('#removeAll').should('exist')
     cy.get('#active').should('exist')
+   })
+
+   it('Agregar 2 tareas, completarlas y eliminar la segunda tarea completada',()=>{
+    cy.get('#task').wait(2000).type(data.tareas.tarea1)
+    cy.get('#sendTask').click()
+    cy.get('#task').wait(2000).type(data.tareas.tarea2)
+    cy.get('#sendTask').click()
+    cy.get('li').first().find('p').click()
+    cy.contains('p', 'Tirar la basura').click();
+    cy.wait(5000)
+    cy.contains('p','Tirar la basura').first().next('button').click();
+   })
+   it.only('Agregar 2 tareas y eliminar la primera tarea',()=>{
+    cy.get('#task').wait(2000).type(data.tareas.tarea1)
+    cy.get('#sendTask').click()
+    cy.get('#task').wait(2000).type(data.tareas.tarea2)
+    cy.get('#sendTask').click()
+    cy.get('li').first().find('p').click()
+    cy.contains('p', 'Tirar la basura').click();
+    cy.wait(5000)
+    cy.contains('p','Comprar Pan').first().next('button').click();
    })
 })
